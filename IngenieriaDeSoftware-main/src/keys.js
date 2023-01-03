30352d8a9d2a72372876fc1299c6fa467d38c951
Joel <joelcolinr@gmail.com>
2023-01-03 12:24:49 -0600

    Login Modificado

--- IngenieriaDeSoftware-main/src/keys.js
+++ IngenieriaDeSoftware-main/src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'Batman777',
         database: 'mathebasics'
     }
 };
---
+++ IngenieriaDeSoftware-main/src/public/js/login.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById("btn__registrarse").addEventListener("click", register);
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+
+
+var cont_login_register =  document.querySelector('.contenedor__login-register');
+var form_login = document.querySelector('.formulario__login');
+var form_register = document.querySelector('.formulario__register');
+var caja_trasera_login = document.querySelector('.caja__trasera-login');
+var caja_trasera_register = document.querySelector('.caja__trasera-register');
+
+function register(){
+    form_register.style.display = "block";
+    cont_login_register.style.left = "32rem";
+    form_login.style.display = "none";
+    caja_trasera_register.style.opacity = "0";
+    caja_trasera_login.style.opacity = "1";
+}
+function iniciarSesion(){
+    form_register.style.display = "none";
+    cont_login_register.style.left = "1rem";
+    form_login.style.display = "block";
+    caja_trasera_register.style.opacity = "1";
+    caja_trasera_login.style.opacity = "0";
+}
