654b639cce106cb89ef0872b613afed45957a2e2
Erick CC <erickcasarrubiascruz8@gmail.com>
2023-01-04 17:30:34 -0600

    juegos_mod

--- IngenieriaDeSoftware-main/src/keys.js
+++ IngenieriaDeSoftware-main/src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'root',
-        password: 'Batman777',
+        password: '',
         database: 'mathebasics'
     }
 };
